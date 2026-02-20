dcda5dcec28c56b36421837d578f8ac396a49ab3
Ronan <ronan.poncet@le-campus-numerique.fr>
2026-02-20 10:09:15 +0100

    ajout d'une interface graphique

--- ofApp.cpp
+++ ofApp.cpp
@@ -1,228 +1,238 @@
 #include "ofApp.h"
-#include "Oscillator.h"
+
 //--------------------------------------------------------------
 void ofApp::setup(){
 
     ofBackground(34, 34, 34);
+    ofSetCircleResolution(60);
 
     int bufferSize = 512;
     sampleRate = 44100;
 
+    // Initialisation des variables de phase et fréquence
     phase = 0;
     phaseAdder = 0.0f;
     phaseAdderTarget = 0.0f;
     targetFrequency = 261.63f; // Do par défaut
-	sample = 0;
+    sample = 0;
 
     // ===== ENVELOPPE =====
     volume = 0.0f;
-    targetVolume = 0.1f;   // volume maximum
-    attackSpeed = 0.002f;  // vitesse montée
-    releaseSpeed = 0.002f; // vitesse descente
+    attackSpeed = 0.01f;  
+    releaseSpeed = 0.01f; 
     noteIsOn = false;
 
     pan = 0.5f;
 
-	lowpass = false;
+    // ===== FILTRE IIR =====
+    lowpass = false;
+    xn = xn_1 = xn_2 = 0;
+    yn = yn_1 = yn_2 = 0;
+    f0 = 1000.0f;
 
-	xn = 0;
-	xn_1 = 0;
-	xn_2 = 0;
-	yn = 0;
-	yn_1 = 0;
-	yn_2 = 0;
+    // ===== INTERFACE GRAPHIQUE (GUI) =====
+    gui.setup("Parametres Synth");
+    gui.add(volumeMax.setup("Volume Max", 0.2f, 0.0f, 0.5f));
+    gui.add(brightnessSlider.setup("Brillance", 10.0f, 1.0f, 40.0f));
+    gui.add(cutoffSlider.setup("Filtre Freq (Hz)", 1000.0f, 50.0f, 5000.0f));
+    gui.add(lowpassToggle.setup("Activer Filtre", false));
+    
+    // Initialisation des buffers pour la visualisation
+    uAudio.assign(bufferSize, 0.0);
+    fTransform.assign(bufferSize, 0.0);
+    f1Transform.assign(bufferSize, 0.0);
+    f2Transform.assign(bufferSize, 0.0);
 
-	f0 = 1000.;
+    oscillator.setup(sampleRate);
 
-	lAudio.assign(bufferSize, 0.0);
-	uAudio.assign(bufferSize, 0.0);
-	fTransform.assign(bufferSize, 0.0);
-	f1Transform.assign(bufferSize, 0.0);
-	f2Transform.assign(bufferSize, 0.0);
+    // Configuration du flux audio
+    ofSoundStreamSettings settings;
+    auto devices = soundStream.getMatchingDevices("default");
+    if(!devices.empty()){
+        settings.setOutDevice(devices[0]);
+    }
 
-	oscillator.setup(sampleRate);
-
-	soundStream.printDeviceList();
-
-	ofSoundStreamSettings settings;
-
-	auto devices = soundStream.getMatchingDevices("default");
-	if(!devices.empty()){
-		settings.setOutDevice(devices[0]);
-	}
-
-	settings.setOutListener(this);
-	settings.sampleRate = sampleRate;
-	settings.numOutputChannels = 1;
-	settings.numInputChannels = 0;
-	settings.bufferSize = bufferSize;
-	soundStream.setup(settings);
+    settings.setOutListener(this);
+    settings.sampleRate = sampleRate;
+    settings.numOutputChannels = 1;
+    settings.numInputChannels = 0;
+    settings.bufferSize = bufferSize;
+    soundStream.setup(settings);
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){ }
+void ofApp::update(){
+    // Mise à jour des paramètres de l'oscillateur et du filtre via le GUI
+    oscillator.setBrightness(brightnessSlider);
+    f0 = cutoffSlider;
+    lowpass = lowpassToggle;
+}
 
 //--------------------------------------------------------------
 void ofApp::draw(){
 
-	ofSetColor(225);
-	ofDrawBitmapString("AUDIO OUTPUT EXAMPLE", 32, 32);
-	
-	ofNoFill();
-	
-	// draw the mono channel:
-	ofPushStyle();
-	ofPushMatrix();
-	ofTranslate(32, 150, 0);
-		
-	ofSetColor(225);
-	ofDrawBitmapString("Mono Channel", 4, 18);
-	ofSetLineWidth(1);	
-	ofDrawRectangle(0, 0, 900, 200);
-
-	ofSetColor(245, 58, 135);
-	ofSetLineWidth(3);
-	ofBeginShape();
-	for (unsigned int i = 0; i < uAudio.size(); i++){
-		float x =  ofMap(i, 0, uAudio.size(), 0, 900, true);
-		ofVertex(x, 100 - uAudio[i]*180.0f);
-	}
-	ofEndShape(false);
-	ofPopMatrix();
-	ofPopStyle();
-
-	// draw the FFT:
-	ofPushStyle();
-	ofPushMatrix();
-	ofTranslate(32, 350, 0);
-		
-	ofSetColor(225);
-	ofDrawBitmapString("FFT", 4, 18);
-	ofSetLineWidth(1);	
-	ofDrawRectangle(0, 0, 900, 200);
-
+    ofSetColor(225);
+    ofDrawBitmapString("SYNTHETISEUR INTERACTIF", 32, 32);
+    
+    // Dessin de la forme d'onde (Mono Channel)
+    ofPushStyle();
+    ofPushMatrix();
+    ofTranslate(32, 100, 0);
+    ofSetColor(225);
+    ofDrawBitmapString("Signal Temporel", 4, 18);
+    ofNoFill();
+    ofDrawRectangle(0, 0, 900, 150);
     ofSetColor(245, 58, 135);
-    ofSetLineWidth(3);
+    ofSetLineWidth(2);
     ofBeginShape();
-    for (unsigned int i = 0; i < fTransform.size() / 2; i++){
-        float x = ofMap(i, 0, fTransform.size() / 2, 0, 900, true);
-        ofVertex(x, 100 - fTransform[i] * 180.0f);
+    for (unsigned int i = 0; i < uAudio.size(); i++){
+        float x = ofMap(i, 0, uAudio.size(), 0, 900, true);
+        ofVertex(x, 75 - uAudio[i] * 120.0f);
     }
     ofEndShape(false);
     ofPopMatrix();
+    ofPopStyle();
 
+    // Dessin de la FFT
+    ofPushStyle();
+    ofPushMatrix();
+    ofTranslate(32, 280, 0);
     ofSetColor(225);
-	string reportString = string("Envelope synth\n") // Wrap the first one in string()
-						+ "Current volume: " + ofToString(volume, 2) + "\n"
-						+ "Frequency: " + ofToString(targetFrequency, 2) + " Hz\n"
-						+ "Changer d'octave (up arrow / down arrow) \n"
-						+ "White Keys: a z e r t y u" + "\n"
-						+ "Black Keys: s d f g h" + "\n"
-						+ "Brillance (+-)" + ofToString(oscillator.brightness) + "\n"
-						+ "Forme d'onde (w)" + ofToString(oscillator.waveform) + "\n"
-						+ "Filtre passe bas activation (l): " + ofToString(lowpass) + "\n"
-						+ "Filtre passe bas frequency (*/): " + ofToString(f0);
-    ofDrawBitmapString(reportString, 32, 579);
+    ofDrawBitmapString("Analyse Frequentielle (FFT)", 4, 18);
+    ofNoFill();
+    ofDrawRectangle(0, 0, 900, 150);
+    ofSetColor(0, 191, 255);
+    ofBeginShape();
+    for (unsigned int i = 0; i < fTransform.size() / 2; i++){
+        float x = ofMap(i, 0, fTransform.size() / 2, 0, 900, true);
+        ofVertex(x, 140 - fTransform[i] * 150.0f);
+    }
+    ofEndShape(false);
+    ofPopMatrix();
+    ofPopStyle();
+
+    // Affichage de l'interface GUI
+    gui.draw();
+
+    // Instructions clavier
+    ofSetColor(200);
+    string info = "Clavier: a,z,e,r,t,y,u (Notes) | Up/Down (Octave: " + ofToString(oscillator.octave) + ")\n";
+    info += "Forme d'onde (w): " + ofToString(oscillator.waveform);
+    ofDrawBitmapString(info, 32, 480);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 
-	// oscillator.setWaveform(SQUARE);
-
     float frequency = 0;
+    switch(key){
+        case 'a': frequency = 261.63f; break; 
+        case 'z': frequency = 293.66f; break; 
+        case 'e': frequency = 329.63f; break; 
+        case 'r': frequency = 349.23f; break; 
+        case 't': frequency = 392.00f; break; 
+        case 'y': frequency = 440.00f; break; 
+        case 'u': frequency = 493.88f; break; 
+        case 's': frequency = 277.18f; break; 
+        case 'd': frequency = 311.13f; break; 
+        case 'f': frequency = 369.99f; break; 
+        case 'g': frequency = 415.30f; break; 
+        case 'h': frequency = 466.16f; break; 
+    }
 
-	switch(key){
-		// touches blanches
-		case 'a': frequency = 261.63f; break; // Do
-		case 'z': frequency = 293.66f; break; // Ré
-		case 'e': frequency = 329.63f; break; // Mi
-		case 'r': frequency = 349.23f; break; // Fa
-		case 't': frequency = 392.00f; break; // Sol
-		case 'y': frequency = 440.00f; break; // La
-		case 'u': frequency = 493.88f; break; // Si
-
-		// touches noires (dièses)
-		case 's': frequency = 277.18f; break; // Do#
-		case 'd': frequency = 311.13f; break; // Ré#
-		case 'f': frequency = 369.99f; break; // Fa#
-		case 'g': frequency = 415.30f; break; // Sol#
-		case 'h': frequency = 466.16f; break; // La#
-	}
-		if(key == 'w'){
+    if(key == 'w'){
         if(oscillator.waveform == SINE) oscillator.setWaveform(SQUARE);
         else if(oscillator.waveform == SQUARE) oscillator.setWaveform(SAW);
         else if(oscillator.waveform == SAW) oscillator.setWaveform(TRIANGLE);
         else if(oscillator.waveform == TRIANGLE) oscillator.setWaveform(SINE);
     }
 
-	if (key == 'l')
-	{
-		if (lowpass)
-		{
-			lowpass = false;
-		} else {
-			lowpass = true;
-		}
-	}
-	
-	if (lowpass)
-	{
-		if (key == '*' && f0 < sampleRate/2 - 100)
-		{
-			f0 += 100.;
-		}
-		if (key == '/' && f0 > 99)
-		{
-			f0 -= 100.;
-		}
-	}
+    if(key == OF_KEY_UP && oscillator.octave < 8) oscillator.octave++;
+    if(key == OF_KEY_DOWN && oscillator.octave > 0) oscillator.octave--;
 
-		if(key == OF_KEY_UP){
-			if(oscillator.octave < 10){
-			oscillator.octave +=1;
-		}
-	}
-		if(key == OF_KEY_DOWN){
-			if(oscillator.octave != 0){
-			oscillator.octave -=1;
-		}
-	}
-
-
-
-	if(key == '+'){
-
-		oscillator.setBrightness(oscillator.brightness + 0.1);
-	}
-
-	if(key == '-'){
-		if(oscillator.brightness == 0){}
-		else{
-		oscillator.setBrightness(oscillator.brightness - 0.1);
-		}
-	}
     if(frequency > 0){
-        targetFrequency = frequency *std::pow(2,oscillator.octave);
-        phaseAdderTarget = (targetFrequency / (float)sampleRate) * glm::two_pi<float>();
-        noteIsOn = true; // déclenche Attack
+        targetFrequency = frequency * std::pow(2, oscillator.octave);
+        oscillator.setFrequency(targetFrequency);
+        noteIsOn = true; 
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
+    noteIsOn = false;
+}
 
-    switch(key){
-        case 'a': case 'z': case 'e': case 'r':
-        case 't': case 'y': case 'u': case 's': 
-		case 'd': case 'f': case 'g': case 'h':
-            noteIsOn = false; // déclenche Release
-            break;
+//--------------------------------------------------------------
+void ofApp::audioOut(ofSoundBuffer & buffer){
+
+    for (size_t i = 0; i < buffer.getNumFrames(); i++){
+
+        // Gestion de l'enveloppe
+        if(noteIsOn){
+            volume += attackSpeed;
+            if(volume > volumeMax) volume = (float)volumeMax;
+        } else {
+            volume -= releaseSpeed;
+            if(volume < 0.0f) volume = 0.0f;
+        }
+
+        // Génération du signal
+        sample = oscillator.getNextSample();
+        
+        // Filtrage IIR
+        xn_2 = xn_1;
+        xn_1 = xn;
+        xn = sample;
+
+        yn_2 = yn_1;
+        yn_1 = yn;
+        yn = computeIIRFilter();
+
+        float finalSample = lowpass ? yn : sample;
+        buffer[i] = finalSample * volume;
+        
+        uAudio[i] = buffer[i]; 
+    }
+    computeFourierTransform(buffer);
+}
+
+//--------------------------------------------------------------
+float ofApp::computeIIRFilter() {
+    float Fs = sampleRate;
+    float wo = glm::two_pi<float>() * (f0 / Fs);
+    float Q  = 1.0f;
+    float alpha = sin(wo) / (2.0f * Q);
+    
+    float b0 = alpha;
+    float b1 = 0;
+    float b2 = -alpha;
+    float a0 = 1 + alpha;
+    float a1 = -2 * cos(wo);
+    float a2 = 1 - alpha;
+
+    return (b0/a0) * xn + (b1/a0) * xn_1 + (b2/a0) * xn_2 - (a1/a0) * yn_1 - (a2/a0) * yn_2;
+}
+
+//--------------------------------------------------------------
+void ofApp::computeFourierTransform(const ofSoundBuffer& buffer) {
+    size_t N = buffer.getNumFrames();
+    for (size_t i = 0; i < N; i++) {
+        f1Transform[i] = 0;
+        f2Transform[i] = 0;
+        for (size_t j = 0; j < N; j++) {
+            float angle = 2 * PI * i * j / N;
+            f1Transform[i] += uAudio[j] * cos(angle);
+            f2Transform[i] += uAudio[j] * sin(angle);
+        }
+    }
+    for (size_t i = 0; i < N; i++) {
+        fTransform[i] = 5 * sqrt(f1Transform[i]*f1Transform[i] + f2Transform[i]*f2Transform[i]) / N;
     }
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
+void ofApp::mouseMoved(int x, int y ){
+    // Optionnel : mettre à jour le pan ici si vous le souhaitez
     pan = (float)x / (float)ofGetWidth();
 }
 
@@ -232,99 +242,21 @@ void ofApp::mouseDragged(int x, int y, int button){
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){ }
-
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){ }
-
-//--------------------------------------------------------------
-void ofApp::audioOut(ofSoundBuffer & buffer){
-
-    while (phase > glm::two_pi<float>()){
-        phase -= glm::two_pi<float>();
-    }
-
-    phaseAdder = 0.95f * phaseAdder + 0.05f * phaseAdderTarget;
-
-    for (size_t i = 0; i < buffer.getNumFrames(); i++){
-
-        // ===== ENVELOPPE ATTACK / RELEASE =====
-        if(noteIsOn){
-            volume += attackSpeed;
-            if(volume > targetVolume)
-                volume = targetVolume;
-        }else{
-            volume -= releaseSpeed;
-            if(volume < 0.0f)
-                volume = 0.0f;
-        }
-
-        phase += phaseAdder;
-		oscillator.setFrequency(targetFrequency);
-
-		xn_2 = xn_1;
-		xn_1 = xn;
-
-		yn_2 = yn_1;
-		yn_1 = yn;
-
-        sample = oscillator.getNextSample();
-		xn = sample;
-
-		yn = computeIIRFilter();
-
-		if (lowpass)
-		{
-			buffer[i] = yn * volume;
-		} else {
-			buffer[i] = sample * volume;
-		}
-		
-        lAudio[i] = buffer[i];
-        uAudio[i] = buffer[i];
-    }
-
-    computeFourierTransform(buffer);
+void ofApp::mousePressed(int x, int y, int button){
+    // Logique au clic si nécessaire
 }
 
 //--------------------------------------------------------------
-void ofApp::computeFourierTransform(const ofSoundBuffer& buffer) {
-
-	size_t N = buffer.getNumFrames();
-
-	for (size_t i = 0; i < N; i++) {
-		f1Transform[i] = 0;
-		f2Transform[i] = 0;
-
-		for (size_t j = 0; j < N; j++) {
-			float angle = 2 * PI * i * j / N;
-			f1Transform[i] += uAudio[j] * cos(angle);
-			f2Transform[i] += uAudio[j] * sin(angle);
-		}
-	}
-
-	for (size_t i = 0; i < N; i++) {
-		fTransform[i] = 5 * sqrt(f1Transform[i]*f1Transform[i] + f2Transform[i]*f2Transform[i]) / N;
-	}
-}
-
-float ofApp::computeIIRFilter() {
-	Fs = sampleRate;
-	wo = glm::two_pi<float>() * (f0 / Fs);
-	Q  = 1;
-	a  = sin(wo)/(2*Q);
-	b0 = a;
-	b1 = 0;
-	b2 = -a;
-	a0 = 1 + a;
-	a1 = -2 * cos(wo);
-	a2 = 1 - a;
-	return (b0/a0) * xn + (b1/a0) * xn_1 + (b2/a0) * xn_2 - (a1/a0) * yn_1 - (a2/a0) * yn_2;
+void ofApp::mouseReleased(int x, int y, int button){
+    // Logique au relâchement si nécessaire
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){ }
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+    // Gestion du glisser-déposer de fichiers
+}
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ }
+void ofApp::gotMessage(ofMessage msg){
+    // Gestion des messages système
+}
--- ofApp.h
+++ ofApp.h
@@ -2,7 +2,7 @@
 
 #include "ofMain.h"
 #include "Oscillator.h"
-#include "def.h"
+#include "ofxGui.h" // Nécessaire pour l'interface graphique
 
 class ofApp : public ofBaseApp{
 
@@ -36,43 +36,36 @@ class ofApp : public ofBaseApp{
 		vector<float> f1Transform;
 		vector<float> f2Transform;
 
-		// SINE WAVE
+		// SYNTH & PHASE
 		float targetFrequency;
 		float phase;
 		float phaseAdder;
 		float phaseAdderTarget;
 		float sample;
 
-
 		// ===== ENVELOPPE =====
-		float volume;        // volume courant
-		float targetVolume;  // volume max
+		float volume;        // volume courant (pour le lissage)
 		float attackSpeed;   // vitesse montée
 		float releaseSpeed;  // vitesse descente
 		bool  noteIsOn;      // état de la note
 
 		Oscillator oscillator;
 
-		// filter
+		// ===== FILTRE IIR =====
 		bool lowpass;
+		float f0, Fs, wo, Q, a;  
+		float b0, b1, b2, a0, a1, a2; 
+		float xn, xn_1, xn_2;
+		float yn, yn_1, yn_2;
 
-		float f0; 
-		float Fs; 
-		float wo; 
-		float Q;  
-		float a;  
-		float b0; 
-		float b1; 
-		float b2; 
-		float a0; 
-		float a1; 
-		float a2; 
-
-		float xn;
-		float xn_1;
-		float xn_2;
-		float yn;
-		float yn_1;
-		float yn_2;
+		// ==========================================
+		// INTERFACE GRAPHIQUE (GUI)
+		// ==========================================
+		ofxPanel gui;                    // Panneau principal
+		ofxFloatSlider volumeMax;        // Slider pour le volume max
+		ofxFloatSlider brightnessSlider; // Slider pour la brillance de l'oscillateur
+		ofxFloatSlider cutoffSlider;     // Slider pour la fréquence du filtre
+		ofxToggle lowpassToggle;         // Interrupteur pour activer/désactiver le filtre
+		// ==========================================
 		
 };
